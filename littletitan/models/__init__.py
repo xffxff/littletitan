81846c4df913c59a6db24ec06ec43547cb587f2e
xffxff <1247714429@qq.com>
2025-01-17 10:28:11 +0800

    add config for deepseek moe 16b

--- littletitan/models/__init__.py
+++ littletitan/models/__init__.py
@@ -4,6 +4,18 @@ from littletitan.models.model import Transformer, ModelArgs
 
 moe_configs = {
     "debugmodel": ModelArgs(dim=256, n_layers=8, n_heads=16, rope_theta=500000),
+    "deepseek-moe-16b": ModelArgs(
+        dim=2048,
+        n_layers=12, # 28, set to 12 for local testing
+        n_heads=16,
+        n_kv_heads=16,
+        multiple_of=128,
+        max_seq_len=4096,
+        moe_expert_dim=1408,
+        moe_num_experts=64,
+        moe_top_k=6,
+        moe_num_shared_experts=2,
+    )
 }
 
 
